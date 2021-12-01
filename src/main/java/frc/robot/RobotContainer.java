573dd5d748c542e73bc4b5e0424b36a51543757f
SteelTalonRoboticsLaptop <frc6910@gmail.com>
2021-12-01 16:16:36 -0500

    fixed constants & added vendordeps

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,11 +7,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drivetrain;
-import edu.wpi.first.wpilibj2.command.Command;
-
-import Constants.*;
+import frc.robot.Constants;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
